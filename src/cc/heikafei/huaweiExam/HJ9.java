6421a4054f8fc904ca83b76697d4e7d4492c7517
ning <1471723394@qq.com>
2022-03-24 00:13:56 +0800

    add annotation

--- src/cc/heikafei/huaweiExam/HJ9.java
+++ src/cc/heikafei/huaweiExam/HJ9.java
@@ -10,14 +10,15 @@ import java.util.Scanner;
 public class HJ9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //使用HashSet来判断是否重复
         while (sc.hasNext()) {
             HashSet<Integer> hs = new HashSet<>();
-            int target = sc.nextInt();
-            while (target != 0) {
+            int target = sc.nextInt();      //获取待求解的值
+            while (target != 0) {       //求解每位上面的整数
                 int temp = target % 10;
-                if (hs.add(temp))
+                if (hs.add(temp))       //如果能加入，则说明没有重复
                     System.out.print(temp);
-                target /= 10;
+                target /= 10;       //除10-->去掉最右边的数字
             }
             System.out.println();
         }
