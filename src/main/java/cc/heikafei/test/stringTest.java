c1abe2e1d54917f34bdcec181542820222b70f21
ning <1471723394@qq.com>
2022-03-26 09:48:03 +0800

    stringTest

---
+++ src/main/java/cc/heikafei/test/stringTest.java
@@ -0,0 +1,17 @@
+package cc.heikafei.test;
+
+import java.util.Locale;
+
+/**
+ * 字符串测试
+ */
+public class stringTest {
+    public static void main(String[] args) {
+        String str = "hello";
+        System.out.println(str);
+
+        //将字符串全部转为大写
+        str = str.toUpperCase();
+        System.out.println(str);
+    }
+}
