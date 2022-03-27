a78bccc4657caf8ae4c3924df6adf87916e85a50
ning <1471723394@qq.com>
2022-03-27 11:47:44 +0800

    modify something in stringTest

--- src/main/java/cc/heikafei/test/stringTest.java
+++ src/main/java/cc/heikafei/test/stringTest.java
@@ -7,11 +7,33 @@ import java.util.Locale;
  */
 public class stringTest {
     public static void main(String[] args) {
-        String str = "hello";
-        System.out.println(str);
+        String str1 = "hello";
+        System.out.println(str1);       //hello
+
+        //字符串比较
+        String str2 = "hello";
+        System.out.println(str1 == str2);       //true
+        System.out.println(str1.equals(str2));      //true
+
+        //字符串比较二
+        String str3 = "HELLO".toLowerCase();
+        System.out.println(str1 == str3);       //false
+        System.out.println(str1.equals(str3));      //true
 
         //将字符串全部转为大写
-        str = str.toUpperCase();
-        System.out.println(str);
+        str1 = str1.toUpperCase();
+        System.out.println(str1);       //HELLO
+
+        //是否有子串
+        System.out.println(str1.contains("he"));        //false
+        System.out.println(str1.contains("HE"));        //true
+
+        //提取子串
+        System.out.println(str1.substring(2, 4));       //LL
+        System.out.println(str1.substring(2));      //LLO
+
+        //去除字符串首位空白字符
+        System.out.println(" \thello,ning\r\n".trim());
+
     }
 }
