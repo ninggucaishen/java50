62beafe9af156c6853dfe2460f130f95de19a992
ning <1471723394@qq.com>
2022-03-27 22:04:51 +0800

    update something in stringTest

--- src/main/java/cc/heikafei/test/stringTest.java
+++ src/main/java/cc/heikafei/test/stringTest.java
@@ -1,6 +1,7 @@
 package cc.heikafei.test;
 
 import java.util.Locale;
+import java.util.StringJoiner;
 
 /**
  * 字符串测试
@@ -34,6 +35,33 @@ public class stringTest {
 
         //去除字符串首位空白字符
         System.out.println(" \thello,ning\r\n".trim());
+
+        //字符串拼接
+        String str4 = "";
+        for (int i = 0; i < 10; i++) {
+            str4 = str4 + "," + i;
+        }
+        System.out.println(str4);       //,0,1,2,3,4,5,6,7,8,9
+
+        //字符串拼接二
+        StringBuilder sb = new StringBuilder(1024);
+        for (int i = 0; i < 10; i++) {
+            sb.append(',');
+            sb.append(i);
+        }
+        System.out.println(sb);     //,0,1,2,3,4,5,6,7,8,9
+
+        //字符串拼接三
+        String[] names = {"Alice", "Bob", "Jane"};
+        StringJoiner sj = new StringJoiner(",", "hello,", "!");
+        for (String name : names) {
+            sj.add(name);
+        }
+        System.out.println(sj);     //hello,Alice,Bob,Jane!
+
+        //字符串拼接四
+        String join = String.join(", ", names);
+        System.out.println(join);       //Alice, Bob, Jane
 
     }
 }
