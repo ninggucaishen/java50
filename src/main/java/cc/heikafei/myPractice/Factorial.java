963c50d84abbf3545adae2d68f68bcc0ab552d78
ning <1471723394@qq.com>
2022-03-28 11:17:26 +0800

    update JUnit test

--- src/main/java/cc/heikafei/myPractice/Factorial.java
+++ src/main/java/cc/heikafei/myPractice/Factorial.java
@@ -6,6 +6,9 @@ package cc.heikafei.myPractice;
  */
 public class Factorial {
     public static long fact(long n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
         long r = 1;
         for (long i = 1; i <= n; i++) {
             r = r * i;
--- src/test/java/cc/heikafei/myPractice/FactorialTest.java
+++ src/test/java/cc/heikafei/myPractice/FactorialTest.java
@@ -3,12 +3,28 @@ package cc.heikafei.myPractice;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 
 /**
  * JUnit测试
  */
 public class FactorialTest {
 
+    Factorial factorial;
+
+    @BeforeEach
+    public void setUp() {
+        this.factorial = new Factorial();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.factorial = null;
+    }
+
+    @Disabled       //条件测试
     @Test
     public void testFactorial() {
         assertEquals(1, Factorial.fact(1));
@@ -17,4 +33,12 @@ public class FactorialTest {
         assertEquals(3628800, Factorial.fact(10));
         assertEquals(2432902008176640000L, Factorial.fact(20));
     }
+
+    @Test
+    public void testNegative() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Factorial.fact(-1);
+        });
+    }
+
 }
