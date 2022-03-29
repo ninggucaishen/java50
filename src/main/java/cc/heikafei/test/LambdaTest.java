4247aa3e0673a6e1ec138857bc6695ad5ce3026c
ning <1471723394@qq.com>
2022-03-29 14:49:01 +0800

    ...

--- src/main/java/cc/heikafei/test/LambdaTest.java
+++ src/main/java/cc/heikafei/test/LambdaTest.java
@@ -1,9 +1,11 @@
 package cc.heikafei.test;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * Lambda表达式测试
@@ -39,11 +41,20 @@ public class LambdaTest {
         list.forEach(System.out::println);
 
         List<Integer> prims = Arrays.asList(2, 4, 5, 3, 6, 1, 8, 3, 9, 4);
-        IntSummaryStatistics stats = prims.stream().mapToInt((x) -> x)
-                .summaryStatistics();
+        IntSummaryStatistics stats = prims.stream().mapToInt((x) -> x).summaryStatistics();
         System.out.println("最大值为：" + stats.getMax());
         System.out.println("最小值为：" + stats.getMin());
         System.out.println("总和为：" + stats.getSum());
         System.out.println("平均值为：" + stats.getAverage());
+
+        //String转Integer
+        Function<String, Integer> function = Integer::parseInt;
+        Integer num = function.apply("1");
+        System.out.println(num);
+
+        Function<Integer, String> fun = String::valueOf;
+        String str = fun.toString();
+        System.out.println(str);
+
     }
 }
