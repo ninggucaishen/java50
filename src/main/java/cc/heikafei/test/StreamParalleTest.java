b938dab714ce55f6cf133abfed8be895058a1873
ning <1471723394@qq.com>
2022-04-03 19:35:48 +0800

    reduce test

--- src/main/java/cc/heikafei/test/StreamParalleTest.java
+++ src/main/java/cc/heikafei/test/StreamParalleTest.java
@@ -1,6 +1,8 @@
 package cc.heikafei.test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -11,6 +13,20 @@ public class StreamParalleTest {
     private static int total = 100000000;
 
     public static void main(String[] args) {
+
+//        streamPrarlle();
+
+        reduceTest();
+
+    }
+
+    private static void reduceTest() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Integer sum = list.stream().reduce(0, (a, b) -> a + b);
+        System.out.println(sum);
+    }
+
+    private static void streamPrarlle() {
         System.out.println(String.format("本计算机的核数：%d", Runtime.getRuntime().availableProcessors()));
 
         //产生1000w个随机数（1-100），组成列表
@@ -29,7 +45,6 @@ public class StreamParalleTest {
         //只需加上.parallel()就行了
         list.stream().parallel().reduce((a, b) -> a + b).ifPresent(System.out::println);
         System.out.println(String.format("多线程计算耗时：%d", getCurrentTime() - prevTime));
-
     }
 
     private static long getCurrentTime() {
