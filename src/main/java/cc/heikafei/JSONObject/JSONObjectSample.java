acd82105bba107ab6255f885d0f536cf0efb7d76
ning <1471723394@qq.com>
2022-04-14 17:12:34 +0800

    JSONObject test

---
+++ src/main/java/cc/heikafei/JSONObject/JSONObjectSample.java
@@ -0,0 +1,42 @@
+package cc.heikafei.JSONObject;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+/**
+ * JSONObject test
+ */
+public class JSONObjectSample {
+    public static void main(String[] args) {
+
+        createJson();
+
+//        createJsonByHashMap();
+    }
+
+    //直接构建
+    private static void createJson() {
+        JSONObject obj = new JSONObject();
+        obj.put("id", "001");
+        obj.put("name", "ning");
+        obj.put("age", "27");
+        obj.put("isStudent", false);
+        obj.put("hobbies", new String[]{"hiking", "swimming", "fishing"});
+        System.out.println(obj);
+    }
+
+    //使用HashMap构建
+    private static void createJsonByHashMap() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("id", "001");
+        map.put("name", "ning");
+        map.put("age", "27");
+        map.put("isStudent", false);
+        map.put("hobbies", new String[]{"hiking", "swimming", "fishing"});
+        System.out.println(map);
+
+        JSONObject obj = new JSONObject(map);
+        System.out.println(obj);
+    }
+}
