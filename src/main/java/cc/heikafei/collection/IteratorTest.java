0e9047c04329759b95c2312cc5e5f71f5175588f
ning <1471723394@qq.com>
2022-04-22 16:35:02 +0800

    Iterator test

---
+++ src/main/java/cc/heikafei/collection/IteratorTest.java
@@ -0,0 +1,33 @@
+package cc.heikafei.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorTest {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+
+        //集合自带的remove()
+        //不要再迭代器迭代过程中使用这个
+        //for (Object o : list) {
+        //    if ("3".equals(o)) {
+        //        list.remove(o);
+        //    }
+        //}
+
+        //迭代器自带的remove()
+        for (Iterator iter = list.iterator(); iter.hasNext(); ) {
+            if ("3".equals(iter.next())) {
+                iter.remove();
+            }
+        }
+
+        System.out.println(list);
+    }
+}
