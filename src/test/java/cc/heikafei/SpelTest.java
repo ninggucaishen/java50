7191183d1dedc397cccc50ba7bb6d4c70bcfa49e
ning <1471723394@qq.com>
2022-07-13 21:47:41 +0800

    test

---
+++ src/test/java/cc/heikafei/SpelTest.java
@@ -0,0 +1,11 @@
+package cc.heikafei;
+
+/**
+ * @ClassName SpelTest
+ * @Description TODO
+ * @Auther Ning
+ * @Date 2022/7/13 18:02
+ * @Version 1.0
+ */
+public class SpelTest {
+}
--- src/test/java/cc/heikafei/nettyTest/MyServerHandler.java
+++ src/test/java/cc/heikafei/nettyTest/MyServerHandler.java
@@ -5,6 +5,7 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @ClassName MyServerHandler
@@ -13,13 +14,38 @@ import io.netty.util.CharsetUtil;
  * @Date 2022/7/13 16:02
  * @Version 1.0
  */
+
+@Slf4j
 public class MyServerHandler extends ChannelInboundHandlerAdapter {
 
-    @Override
+    /*@Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         //获取客户端发来的消息
         ByteBuf byteBuf = (ByteBuf) msg;
         System.out.println("收到客户端" + ctx.channel().remoteAddress() + "发送消息" + byteBuf.toString(CharsetUtil.UTF_8));
+    }*/
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //获取到线程池eventLoop，添加线程，执行
+        ctx.channel().eventLoop().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("延迟5秒");
+                    //长时间操作，不至于长时间的业务操作导致Handler阻塞
+                    Thread.sleep(5000);
+                    try {
+                        int a = 10 / 0;
+                    } catch (Exception e) {
+                        log.error("error:" + e.getMessage());
+                    }
+                    System.out.println("长时间的业务处理");
+                } catch (InterruptedException e) {
+                    log.error("错误原因：" + e.getMessage());
+                }
+            }
+        });
     }
 
     @Override
@@ -33,4 +59,5 @@ public class MyServerHandler extends ChannelInboundHandlerAdapter {
         //发生异常，关闭通道
         ctx.close();
     }
+
 }
